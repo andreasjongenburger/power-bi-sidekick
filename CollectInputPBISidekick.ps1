e0896c0b5e17867dbe5e217fb13ba17f82b37fdd
Andreas Jongenburger <andreasjongenburger@yahoo.com>
2026-03-27 00:13:12 +0100

    New version - also for PBIP and PBIR format
    Select both PBIX and PBIP files
    Ready for consuming PBIR format

--- CollectInputPBISidekick.ps1
+++ CollectInputPBISidekick.ps1
@@ -1,54 +1,41 @@
 #When path is too long:
 #https://www.howtogeek.com/266621/how-to-make-windows-10-accept-file-paths-over-260-characters/
 
-$pathSearchDir = 
-   'C:\DemoFiles\',
-   "$($env:USERPROFILE)\"
+#Add paths of where to find pbix or pbip reports that you want to include in the search step:
+$pathSearchDir = @(
+    'C:\DemoFiles\'
+    "$env:USERPROFILE\Documents\"
+)
 
-$pbiInstances = Get-Process PBIDesktop
-#$runningPBIXfiles = $pbiInstances.MainWindowTitle.Replace(' - Power BI Desktop', '.pbix')
-$runningPBIXfiles = 
-foreach ( $pbiInstance in $pbiInstances )
-{
-    if ($pbiInstance.MainWindowTitle.EndsWith(' - Power BI Desktop') ) 
-    {
-        $pbiInstance.MainWindowTitle.Replace(' - Power BI Desktop', '.pbix')
-    }
-    else 
-    { 
-        $pbiInstance.MainWindowTitle | ForEach-Object {"$_.pbix"}
-    }
-}
+$runningNames = Get-Process PBIDesktop -ErrorAction SilentlyContinue |
+    Where-Object MainWindowTitle |
+    ForEach-Object {
+        $_.MainWindowTitle -replace ' - Power BI Desktop$',''
+    } |
+    Select-Object -Unique
+
+$allPbiFiles = Get-ChildItem -Path $pathSearchDir -Recurse -File -Include '*.pbix','*.pbip' -ErrorAction SilentlyContinue
+
+$openPbiFilesWithPath = $allPbiFiles |
+    Where-Object { $_.BaseName -in $runningNames } |
+    Select-Object -ExpandProperty FullName
 
 
-$allPbiFiles = (Get-ChildItem -Path $pathSearchDir -Recurse -Include '*.pbix').FullName
-#$allPbiFiles = (Get-ChildItem -Recurse -Include '*.pbix').FullName
 
-$openPBIXfilesWithPath = 
-foreach ( $pbixfile in $allPbiFiles )
-{
-    foreach ( $openFile in $runningPBIXfiles )
-    {
-        if ($pbixfile.EndsWith('\'+$openFile) ) 
-        {
-            $pbixfile
-        }
-    }
-}
 $introtext = "------------------------------------------------------------------------------`nWelcome to Power BI Sidekick!`nSee https://andreasjongenburger.com/power-bi-sidekick/ for more information.`n------------------------------------------------------------------------------"
 Write-Host $introtext
-Write-Host "`nThe exact path and name of the current pbix-file is required."
+Write-Host "`nThe exact path and name of the current pbix or pbip file is required."
 Write-Host "To prevent typos, a list of files with filenames currently running is provided here:"
 #Write-Host "At present - from the provided folders - these Power BI files are detected:"
 $listNr = 1
-foreach ($i in $openPBIXfilesWithPath )
+foreach ($i in $openPbiFilesWithPath )
 {
 	Write-Host $listNr":   "$i
 	$listNr++
 }
 
 [int]$correctFileNr = Read-Host "Please enter the number with the current filename and path"
-$PathFile = if ( $openPBIXfilesWithPath.Count -eq 1 ) { $openPBIXfilesWithPath } else { $openPBIXfilesWithPath[$correctFileNr-1] }
+$PathFile = if ( $openPbiFilesWithPath.Count -eq 1 ) { $openPbiFilesWithPath } else { $openPbiFilesWithPath[$correctFileNr-1] }
 $step1result = $introtext + "`n`n  1) Path and filename: " + $PathFile
 clear
 Write-Host $step1result "`n____"
@@ -56,15 +43,14 @@ Write-Host $step1result "`n____"
 
 Write-Host "`nNext, choose one of the available scenario's:"
 Write-Host "1: For reviewing this local file only"
-Write-Host "2: For reviewing all pbix-files in this folder (simulating a live connection scenario)"
+Write-Host "2: For reviewing all pbix-files OR pbip-files (not combined) in this folder (simulating a live connection scenario)"
 Write-Host "3: For this file being a thin report connected to a Premium (per User) dataset"
-Write-Host "4: For reviewing multiple thin reports online - current file should be the dataset-file (also for Pro)"
-Write-Host "5: For this file being a thin report connected through a SSAS tabular live connection"
-Write-Host "6: For reviewing all pbix-files in this folder being thin reports connected through a SSAS tabular live connection"
+Write-Host "4: For this file being a thin report connected through a SSAS tabular live connection"
+Write-Host "5: For reviewing all pbix-files OR pbip-files (not combined) in this folder being thin reports connected through a SSAS tabular live connection"
 
 [int]$scenarioNumber = Read-Host "Please enter the number with the relevant scenario"
 
-$scenarioText = if ( $scenarioNumber -eq 1 ) { 'Only current file' } elseif ( $scenarioNumber -eq 2 ) { 'Live connection simulation' } elseif ( $scenarioNumber -eq 3 ) { 'Thin report connected to Premium (per user) dataset' } elseif ( $scenarioNumber -eq 5 ) { 'Thin report connected to SSAS tabular live connection' } elseif ( $scenarioNumber -eq 6 ) { 'Multiple thin reports connected to SSAS tabular live connection' } else { 'Local dataset file with thin reports in Power BI Service (Api connection)' }
+$scenarioText = if ( $scenarioNumber -eq 1 ) { 'Only current file' } elseif ( $scenarioNumber -eq 2 ) { 'Live connection simulation' } elseif ( $scenarioNumber -eq 3 ) { 'Thin report connected to Premium (per user) dataset' } elseif ( $scenarioNumber -eq 4 ) { 'Not one of the options provided' }
 
 
 $step2result = $step1result + "`n  2) Scenario: " + $scenarioText
@@ -72,7 +58,7 @@ clear
 Write-Host $step2result "`n____"
 
 
-if ( $scenarioNumber -in 5, 6  )
+if ( $scenarioNumber -in 4, 5  )
 {
     $serverName = Read-Host "`nPlease enter the SSAS serverName"
 	clear
@@ -101,36 +87,8 @@ else
 	$DatabaseName = $args[1]
 }
 
-if ( $scenarioNumber -eq 4  )
-{
-    Write-Host "`nFor this scenario, a bit extra input is required."
-	Write-Host "A connection to the Power BI Service will be built using the Rest API."
-	Write-Host "To retrieve all thin reports that user has access to (cross-workspace) the dataset ID is needed."
-	$datasetID = Read-Host "Please enter the dataset id"
-	clear
-	$step3bresult = $step2result + "`n  3) Dataset ID: " + $datasetID
-	Write-Host $step3bresult "`n____"
-}
-else
-{
-	$datasetID = "n/a"
-}
+$datasetID = "n/a"
 
-# Specify the path to tokenfile
-$tokenFile = 	'C:\PBISidekickTemp\token.txt'
-
-if ( $scenarioNumber -eq 4  )
-{
-    Set-Content $tokenFile "Paste bearer token in this file. Get it here: https://learn.microsoft.com/en-us/rest/api/power-bi/datasets/get-datasets#code-try-0. `n`nUse copy button on top right of code snippet, then here Ctrl+A and Ctrl+V`n`nIt will look like this:`nGET https://api.powerbi.com/v1.0/myorg/datasets `nAuthorization: Bearer <long tokenstring>"
-	Write-Host "`nFurthermore, a bearer token is required to connect to the API."
-	Read-Host "After hitting enter, a text file will open where the bearer token can be pasted into"
-	start $tokenFile
-	Read-Host "Is the file with the bearer token saved and closed? Hit enter to continue"
-	clear
-	$step4bresult = $step3bresult + "`n  4) The bearer token is pasted into 'token.txt'"
-	Write-Host $step4bresult "`n____"
-}
-else { Set-Content $tokenFile "n/a" }
 
 $powerbiPBIT = 'C:\PBISidekickTemp\PowerBI Sidekick.pbit'
 
@@ -150,11 +108,6 @@ $stepLastResult =
 	{ 
 		clear
 		Write-Host $step5aresult "`n____"
-	} 
-	elseif ( $scenarioNumber -eq 4 ) 
-	{ 
-		clear
-		Write-Host $step5bresult "`n____"
 	} 
 	else 
 	{ 
@@ -178,5 +131,5 @@ if ( $openPBITorNot -eq 1 ) { Invoke-Item $powerbiPBIT } else {  }
 
 # Write input parameters to file
 $DetailsOutputFile = 'C:\PBISidekickTemp\PBIXConnectionDetails.csv'
-$fileOrFolderOrApi = if ( $scenarioNumber -in 2, 6 ) { 'Folder' } elseif ( $scenarioNumber -eq 4 ) { 'Api' } else { 'File' }
-$serverName + ',' + $DatabaseName + ',' + $PathFile + ',' + $fileOrFolderOrApi + ',' + $datasetID  | Out-File $DetailsOutputFile
+$fileOrFolder = if ( $scenarioNumber -in 2, 6 ) { 'Folder' } else { 'File' }
+$serverName + ',' + $DatabaseName + ',' + $PathFile + ',' + $fileOrFolder + ',' + $datasetID  | Out-File $DetailsOutputFile
